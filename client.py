3be68d06ab58cd3d9136f7c740055123ca16ce0c
Subject-one <clashez@live.com>
2021-10-02 19:41:57 +1000

    Docstrings added.

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import socket
-import sys
+from sys import exit
 
 SERVER = '127.0.0.1'
 PORT = 6050
@@ -12,6 +12,25 @@ SEPARATOR = "<SEPARATOR>".encode(FORMAT)
 
 class Client:
     def __init__(self):
+        """Begins the process for connecting a client to the server.
+
+        Asks for user input to setup a username.
+        Sets the server to IPv4 TCP. 
+        Send connection request.
+        Set blocking or non-blocking mode of the socket: when false, the socket is set to non-blocking.
+        Print notification to client that they connected.
+        
+        Username is set to encode to UTF-8.
+        Username header is checked for length of header encoded to UTF-8.
+        Client socket is sent with users information (header and username).
+
+        Args:
+            self: create the instance of the class.
+
+        Except:
+            Allows for user to CTRL-C out of program.
+            Runs the destroy() method when called this way to handle KeyboardError.
+        """
         try:
             self.set_username = input("Enter your username: ")
             self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,6 +48,34 @@ class Client:
 
 
     def comms(self):
+        """Handles client side outgoing communications.
+
+        Starts a while loop that is set to True.
+        Try: 
+            Set a variable message that will take input from user.
+            If input is True, runs send_msg() function with message.
+    
+
+        Try: 
+            While loop True is set.
+            Username header is set to client socket for received header constant.
+            If length doesn't exist in username header:
+            Print "Termination of connection" and exit the program.
+            This happens when the server is shut down.
+            The client side receives the notification and lets the client know this way.
+
+        Args:
+            self: create the instance of the class.
+
+        Except:
+            BlockingIOError is handled.
+            It is raised when an operation would block an object, such as a socket.
+            Setblocking is set to False which is why the exception is being handled.
+
+            AttributeError.
+            Raised when an attribute reference fails.
+
+        """
         while True:
             try:
                 message = input(f"{self.set_username}: ")
@@ -42,17 +89,8 @@ class Client:
                 while True:
                     self.username_header = self.client_socket.recv(HEADER)
                     if not len(self.username_header):
-                        print("Aborted Connection")
-                        sys.exit()
-
-                    self.username_length = int(self.username_length.decode(FORMAT).strip())
-                    self.username = self.client_socket.recv(self.username_length).decode(FORMAT)
-
-                    self.message_header = self.client_socket.recv(HEADER)
-                    self.message_length = int(self.message_header.decode(FORMAT).strip())
-                    message = self.client_socket.recv(self.username_length).decode(FORMAT)
-
-                    print(f"{self.username} > {message}")
+                        print("Termination of connection")
+                        exit()
 
             except BlockingIOError:
                 pass
@@ -62,27 +100,57 @@ class Client:
 
 
     def destroy(self, conn=False):
+        """Shuts down the server.
+
+        conn is set to False.
+        If conn is True (connection has been established):
+        Print to client "Disconnecting from server.."
+
+        Removes ^C by backspacing each time chr(8) is used.
+        Gets the character that represents the unicode 8.
+
+        Print to client "has left the server."
+        Runs the shutdown method.
+        Closes the socket.
+        Exits the program.
+
+        Args:
+            self: creates an instance.
+            conn: set to False by default.
+            If it is set to False, it assumes there is no connection established unless True.
+        """
         if conn:
-            # Removes ^C by backspacing each time chr(8) is used.
-            # Gets the character that represents the unicode 8.
             print(chr(8) + chr(8) + "Disconnecting from server..")
             self.send_msg(f"has left the server.")
             self.client_socket.shutdown(1)
             self.client_socket.close()
-            sys.exit()
+            exit()
         else:
             print(chr(8) + chr(8) + "  ")
             print("Closing client...")
-            sys.exit()
+            exit()
 
 
     def send_msg(self, message):
+        """Prepares the client message for delivery to the server.
+
+        Creates a variable message to encode data to UTF-8.
+        Set the message to follow server rules using separator between name and message.
+        Set a message header for outgoing confirmation, encoded to UTF-8.
+        Send the data as client socket.
+
+        Args:
+            self: create the instance of the class.
+            message: the message that the client will be sending to the server.
+
+        Except:
+            If any errors occur, returns False.
+        """
         message = message.encode(FORMAT)
         message = self.username + SEPARATOR + message 
         message_header = f"{len(message):< {HEADER}}".encode(FORMAT)
         self.client_socket.send(message_header + message)
         self.username
-
 
 
 client = Client()
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import socket
 import select
 from datetime import datetime, timedelta
-import sys
+from sys import exit
+
 
 SERVER = '127.0.0.1'
 PORT = 6050
@@ -12,29 +13,40 @@ BUFFER = 1024
 SEPARATOR = "<SEPARATOR>"
 
 
-
 class Message:
     def __init__(self, text):
+        """Initialises for message format.
+
+        init method is called.
+        Takes the text from and removes the b'(bytes literal) after decoding to UTF-8.
+        Sets the separator between the users name and the message sent.
+
+        Args:
+            self: create the instance of the class.
+            text: where the text will be used.
+        """
         self.text = text.decode(FORMAT).strip("b'")
         self.user, separator, self.message = self.text.partition(SEPARATOR)
 
 
     def __repr__(self) -> str:
+        """Initialises for message format.
+
+        repr method is called to to represent the incoming data.
+        Add ": " to the end of the users name.
+        Adds the message after.
+        This format is followed for all incoming messages
+        from clients.
+
+        Args:
+            self: create the instance of the class.
+        """
         return self.user + ": " + self.message
+
 
 
 class Server:
     def __init__(self, server="", port=6050):
-        # Address Family Internet, IPv4, TCP
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # Set, Set Attribute, Set Item True
-        # Will allow users to reconnect to chat
-        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server_socket.bind((server, port))
-        self.active_sockets = [self.server_socket]
-        self.users = {}
-        print(f"Server has successfully launched on {SERVER}")
-        print("=============================================")
         """Begins the setup process for a server.
 
         Sets the server to IPv4 TCP. 
@@ -42,15 +54,38 @@ class Server:
         Bind to the ip and port given in initialise method.
         Holds any open actively connected connections.
         Creates a dictionary for users.
+        Prints a message to the server to let admin know its successfully running.
 
         Args:
             self: create the instance of the class.
             server: leaves an empty string for server IP to fill.
             port: sets the port to connect to.
         """
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((server, port))
+        self.active_sockets = [self.server_socket]
+        self.users = {}
+        print(f"Server has successfully launched on {SERVER}")
+        print("=============================================")
+
 
 
     def receive_msg(self, client_socket):
+        """Uses recv to receive incoming messages from clients.
+
+        The function will attempt to receive a message header from the client.
+        If the client connection can't be established returns False.
+        Set the msg_len variable to be an integer and decode to UTF-8, stripping the ends.
+        returns the received header and client socket as a dictionary.
+
+        Args:
+            self: create the instance of the class.
+            client_socket: the client attempting to connect to the server.
+
+        Except:
+            If any errors occur, returns False.
+        """
         try: 
             receive_header = client_socket.recv(HEADER)
             if not len(receive_header):
@@ -58,17 +93,41 @@ class Server:
             msg_len = int(receive_header.decode(FORMAT).strip())
             return {"header": receive_header, "data": client_socket.recv(msg_len)}
         except:
-            return False
+                return False
 
 
+    # Will remove the b' from text after it is decoded.
     def join_msg_strip(self, text):
         text = text.decode(FORMAT).strip("b'")
         return text
 
 
     def comms(self):
+        """Main body of controlling the servers incoming messages, connections
+           and how to manage them.
+
+        Sets a variable to manage the time with the module datetime.
+        Enters a while Loop that is set to True.
+        Try execute to read sockets in active sockets list.
+        For loop begins. Will accept the connection if the client socket is found
+        in the server socket list.
+        Attempts to read the client message.
+        Appends the active socket from client  to the client socket list.
+        Print a message with the join_msg_strip function to announce in server
+        that the user has joined.
+
+        Set variable message to receive_msg from the client socket.
+        Set users dictionary with access to the client socket to be user.
+        Remove the active socket from the notified sockets list.
+        Set the user to the users dictionary notified socket.
+        Print the time variable.
+        Print the users message, accessing index data from the dictionary.
+
+        Args:
+            self: create the instance of the class.
+        """
         time = (datetime.now() + timedelta(hours=5)).strftime('%H:%M')
-  
+        
         while True:
             try:
             # Read list, write list and error list
@@ -95,28 +154,32 @@ class Server:
                         print(time)
                         print(Message(message['data']))
 
-                        for client_socket in self.users:
-                            if client_socket != notified_socket:
-                                client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
-                    for notified_socket in exception_sockets:
-                        self.active_sockets.remove(notified_socket)
-                        del self.users[notified_socket]
-
             except KeyboardInterrupt:
                 self.destroy()
 
 
     def start(self):
+        # Starts listening for incoming connections
         self.server_socket.listen(5)
 
 
     def destroy(self):
+        """Shuts down the server.
+
+        Runs the shutdown method.
+        Removes ^C by backspacing each time chr(8) is used.
+        Gets the character that represents the unicode 8.
+        Closes the socket.
+        Exits the program.
+
+        Args:
+            self: creates an instance.
+        """
         self.server_socket.shutdown(1)
-        # Removes ^C by backspacing each time chr(8) is used.
-        # Gets the character that represents the unicode 8.
         print(chr(8) + chr(8) + "Server is shutting down...")
         self.server_socket.close()
-        sys.exit()
+        exit()
+
 
 
 server = Server(port=PORT)
